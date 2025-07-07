009e9c5147677bfd77852bf284d40e33a49b98c4
siriusgurban <failoglu96@gmail.com>
2025-07-07 15:44:08 +0400

    added some function in header, hero responsive

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,55 @@
-// "use strict";
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function () {
+  const hamMenu = document.querySelector("#ham_menu");
+  const hamIcon = document.querySelector("#ham_icon");
+  const sideNav = document.querySelector("#sideNav");
+
+  hamMenu.addEventListener("click", function () {
+    hamIcon.classList.toggle("open"); // Toggle hamburger icon state
+    sideNav.classList.toggle("side_nav");
+  });
+});
+
+const sections = document.querySelectorAll("section");
+const navItems = document.querySelectorAll(".nav_item");
+
+window.addEventListener("scroll", () => {
+  let currentSectionId = "";
+
+  sections.forEach((section) => {
+    const rect = section.getBoundingClientRect();
+    console.log(rect, "rect");
+    console.log(window.innerHeight, "window.innerHeight");
+
+    if (
+      rect.top <= window.innerHeight / 2 &&
+      rect.bottom >= window.innerHeight / 2
+    ) {
+      currentSectionId = section.getAttribute("id");
+    }
+  });
+
+  navItems.forEach((item) => {
+    const anchor = item.querySelector("a");
+    console.log(item, "item");
+
+    const href = anchor.getAttribute("href");
+
+    item.classList.remove("nav_active");
+
+    // Check if href matches current section
+    if (href === `#${currentSectionId}`) {
+      item.classList.add("nav_active");
+    }
+
+    // Optional: reset to 'Home' if scrolled to top
+    if (window.scrollY < 200 && href === "./index.html") {
+      item.classList.add("nav_active");
+    }
+  });
+});
+
 
 // const sections = document.querySelectorAll("section");
 // const navItems = document.querySelectorAll(".nav_item");
@@ -38,8 +89,6 @@
 //     }
 //   });
 // });
-
-
 
 // const hamburger = document.querySelector("#hamburger");
 // const sideNav = document.querySelector("#sideNav");
